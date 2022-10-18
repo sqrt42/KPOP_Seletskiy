db9fb2886eaf42c02df0f62d54e31931df19806f
ov3rst33r <sqrtfortytwo@gmail.com>
2022-10-18 16:27:07 +0300

    task 1
    1.1 Basic types

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -1,2 +1,22 @@
-var message = 'Hello World';
-console.log(message);
+"use strict";
+exports.__esModule = true;
+function getAllworkers() {
+    var workers = [
+        { Name: "Ivan", surname: "Ivanov", available: true, salary: 1000 },
+        { Name: "Petro", surname: "Petrov", available: true, salary: 1500 },
+        { Name: "Vasyl", surname: "Vasyliev", available: false, salary: 1600 },
+        { Name: "Evgen", surname: "Zhukov", available: true, salary: 1300 },
+    ];
+    return workers;
+} //returns an array
+function logFirstAvailable(workers) {
+    console.log("Total workers: ".concat(workers.length));
+    for (var _i = 0, workers_1 = workers; _i < workers_1.length; _i++) {
+        var worker = workers_1[_i];
+        if (worker.available) {
+            console.log(worker.Name + " " + worker.surname + " is available");
+            break;
+        }
+    }
+}
+logFirstAvailable(getAllworkers());
--- lab1/lab1.ts
+++ lab1/lab1.ts
@@ -1,2 +1,31 @@
-let message: string = 'Hello World';
-console.log(message);
+function getAllworkers() {
+    let workers = [
+      { Name: "Ivan", surname: "Ivanov", available: true, salary: 1000 },
+      { Name: "Petro", surname: "Petrov", available: true, salary: 1500 },
+      { Name: "Vasyl", surname: "Vasyliev", available: false, salary: 1600 },
+      { Name: "Evgen", surname: "Zhukov", available: true, salary: 1300 },
+    ];
+    return workers;
+  } //returns an array
+  
+  function logFirstAvailable(
+    workers: {
+      Name: string;
+      surname: string;
+      available: boolean;
+      salary: number;
+    }[]
+  ) {
+    console.log(`Total workers: ${workers.length}`);
+  
+    for (let worker of workers) {
+      if (worker.available) {
+        console.log(worker.Name + " " + worker.surname + " is available");
+        break;
+      }
+    }
+  }
+  
+  logFirstAvailable(getAllworkers());
+  
+  export {};
