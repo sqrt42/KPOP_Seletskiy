a9a76b511a133c03abc716e524ea3be14b138d12
ov3rst33r <sqrtfortytwo@gmail.com>
2022-10-18 21:29:53 +0300

    task 2-3
    2.3 Extending interfaces

---
+++ lab2/lab2-3.js
@@ -0,0 +1,13 @@
+var favouriteAuthor = {
+    name: "William Gibson",
+    email: "gibson@gmail.com",
+    numBooksPublished: 13
+};
+var favouriteLibrarian = {
+    name: "Richard Morgan",
+    email: "morgan@gmail.com",
+    department: "Postcyberpunk",
+    assistCustomer: function (custName) {
+        console.log("Morgan");
+    }
+};
---
+++ lab2/lab2-3.ts
@@ -0,0 +1,28 @@
+interface Person { 
+    name: string
+    email: string 
+}
+
+interface Author extends Person {
+    numBooksPublished: number
+}
+
+interface Librarian extends Person {
+    department: string
+    assistCustomer(custName: string): void
+}
+
+let favouriteAuthor: Author = {
+    name: "William Gibson",
+    email: "gibson@gmail.com",
+    numBooksPublished: 13
+}
+
+let favouriteLibrarian: Librarian = {
+    name: "Richard Morgan",
+    email: "morgan@gmail.com",
+    department: "Postcyberpunk",
+    assistCustomer(custName: string): void {
+        console.log("Morgan")
+    }
+}
