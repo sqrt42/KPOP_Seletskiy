54b46c3a94f1f2202428e824ec4d760cdbe64da7
ov3rst33r <sqrtfortytwo@gmail.com>
2022-10-18 16:42:45 +0300

    task 1-2
    1.2 Enum

---
+++ lab1/lab1-2.js
@@ -0,0 +1,60 @@
+"use strict";
+exports.__esModule = true;
+var Category;
+(function (Category) {
+    Category[Category["BusinessAnalisyst"] = 0] = "BusinessAnalisyst";
+    Category[Category["Developer"] = 1] = "Developer";
+    Category[Category["Designer"] = 2] = "Designer";
+    Category[Category["QA"] = 3] = "QA";
+    Category[Category["ScumMaster"] = 4] = "ScumMaster";
+})(Category || (Category = {}));
+function getAllWorkers() {
+    var workers = [
+        {
+            Name: "Ivan",
+            surname: "Ivanov",
+            available: true,
+            salary: 1000,
+            category: Category.ScumMaster
+        },
+        {
+            Name: "Petro",
+            surname: "Petrov",
+            available: true,
+            salary: 1500,
+            category: Category.Developer
+        },
+        {
+            Name: "Vasyl",
+            surname: "Vasyliev",
+            available: false,
+            salary: 1600,
+            category: Category.BusinessAnalisyst
+        },
+        {
+            Name: "Evgen",
+            surname: "Zhukov",
+            available: true,
+            salary: 1300,
+            category: Category.QA
+        },
+    ];
+    return workers;
+}
+function getWorkersNamesByCategory(category) {
+    var surnames = [];
+    var workers = getAllWorkers();
+    for (var _i = 0, workers_1 = workers; _i < workers_1.length; _i++) {
+        var worker = workers_1[_i];
+        if (worker.category === category) {
+            surnames.push(worker.surname);
+        }
+    }
+    return surnames;
+}
+function logWorkersNames(names) {
+    names.forEach(function (name) {
+        console.log(name);
+    });
+}
+logWorkersNames(getWorkersNamesByCategory(Category.ScumMaster));
---
+++ lab1/lab1-2.ts
@@ -0,0 +1,66 @@
+enum Category {
+    BusinessAnalisyst,
+    Developer,
+    Designer,
+    QA,
+    ScumMaster,
+  }
+  
+  function getAllWorkers() {
+    let workers = [
+      {
+        Name: "Ivan",
+        surname: "Ivanov",
+        available: true,
+        salary: 1000,
+        category: Category.ScumMaster,
+      },
+      {
+        Name: "Petro",
+        surname: "Petrov",
+        available: true,
+        salary: 1500,
+        category: Category.Developer,
+      },
+      {
+        Name: "Vasyl",
+        surname: "Vasyliev",
+        available: false,
+        salary: 1600,
+        category: Category.BusinessAnalisyst,
+      },
+      {
+        Name: "Evgen",
+        surname: "Zhukov",
+        available: true,
+        salary: 1300,
+        category: Category.QA,
+      },
+    ];
+  
+    return workers;
+  }
+  
+  function getWorkersNamesByCategory(category: Category): Array<string> {
+    let surnames: Array<string> = [];
+    let workers = getAllWorkers();
+  
+    for (let worker of workers) {
+      if (worker.category === category) {
+        surnames.push(worker.surname);
+      }
+    }
+  
+    return surnames;
+  }
+  
+  function logWorkersNames(names: string[]): void {
+    names.forEach((name) => {
+      console.log(name);
+    });
+  }
+  
+  logWorkersNames(getWorkersNamesByCategory(Category.ScumMaster));
+  
+  export {};
+  
