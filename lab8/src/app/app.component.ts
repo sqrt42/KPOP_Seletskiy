029a0242c2db4e67d0bb358f56bc2835c38fa6aa
ov3rst33r <sqrtfortytwo@gmail.com>
2022-12-07 01:37:12 +0200

    знову шось відвалилось

--- lab8/src/app/app.component.ts
+++ lab8/src/app/app.component.ts
@@ -47,9 +47,9 @@ export class AppComponent implements OnInit{
       })
   }
   checkLink(){
-    if(this.link == ''){ this.errorMsg ='Enter thee link'}
+    if(this.link == ''){ this.errorMsg ='Enter the link'}
     else if(this.link != 'https://randomuser.me/api/?results=50'){
-      this.errorMsg = "Lol I can't work with other APIs, use this one - https://randomuser.me/api/?results=50"
+      this.errorMsg = "I can't work with other APIs, use this one - https://randomuser.me/api/?results=50"
       }
     else{
       this.errorMsg =''
